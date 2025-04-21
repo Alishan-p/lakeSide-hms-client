8ec897b7c69bc666aee143ea593fa1d4905c4c6c
alihsan-p <alishanummer@gmail.com>
2025-04-21 12:09:43 +0530

    feat(room): display all room client side

---
+++ src/components/RoomListing.tsx
@@ -0,0 +1,11 @@
+import Rooms from "./Rooms";
+
+const RoomListing = () => {
+  return (
+    <section className="bg-light p-2 mb-5 mt-5 shadow">
+      <Rooms />
+    </section>
+  );
+};
+
+export default RoomListing;
