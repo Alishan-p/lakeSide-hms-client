eb2d4f240b525b351970fe729ed1b681ba9044e9
alihsan-p <alishanummer@gmail.com>
2025-04-21 13:29:21 +0530

    feat(adminPage): added the admin layout basic

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import ExistingRooms from "./components/ExistingRooms";
+import ExistingRooms from "./components/Room/ExistingRooms";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import EditRoom from "./components/EditRoom";
-import AddRoom from "./components/AddRoom";
+import EditRoom from "./components/Room/EditRoom";
+import AddRoom from "./components/Room/AddRoom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import RoomListing from "./components/RoomListing";
+import RoomListing from "./components/Room/RoomListing";
+import Admin from "./components/Admin/Admin";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           <Route path="/edit-room/:id" Component={EditRoom} />
           <Route path="/add-room" Component={AddRoom} />
           <Route path="/allrooms" Component={RoomListing} />
+          <Route path="/admin" Component={Admin} />
         </Routes>
       </Router>
       <Footer />
